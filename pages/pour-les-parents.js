d551c61172cc79f05c5ed8632727be55536b4ae2
ossthun <msline3@bluewin.ch>
2026-05-31 18:49:43 +0200

    Create pour-les-parents.js page with content

---
+++ pages/pour-les-parents.js
@@ -0,0 +1,26 @@
+import SiteLayout from "../components/SiteLayout";
+
+export default function Page() {
+  return (
+    <SiteLayout title="Pour les parents">
+      <p>
+        Le passé composé peut sembler difficile parce qu’il demande plusieurs
+        étapes. Votre enfant doit choisir un auxiliaire, le conjuguer, puis
+        former le participe passé.
+      </p>
+      <p>
+        Ce site aide à rendre ces étapes visibles. Au lieu de mémoriser une
+        réponse toute faite, l’élève voit comment la forme se construit.
+      </p>
+      <p>
+        Vous pouvez aider votre enfant en lui posant toujours les mêmes
+        questions: quel est le sujet ? Quel est le verbe ? Utilise-t-on avoir ou
+        être ? Quel est le participe passé ?
+      </p>
+      <p>
+        Quelques minutes de pratique régulière sont souvent plus efficaces qu’une
+        longue séance juste avant un test.
+      </p>
+    </SiteLayout>
+  );
+}
