7b46233febd17118cf47422ab079a259337ed84f
ossthun <msline3@bluewin.ch>
2026-05-31 19:02:56 +0200

    Add custom Document component for Next.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5958056334367225"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
