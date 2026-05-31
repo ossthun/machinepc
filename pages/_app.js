df36b2fb4d54d2878fbd57637e057977ccc5d7e4
ossthun <msline3@bluewin.ch>
2026-05-31 19:03:55 +0200

    Remove Google Ads script from _app.js
    Removed Google Ads script from the App component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,5 @@
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        async
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5958056334367225"
-        crossOrigin="anonymous"
-      />
-
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
